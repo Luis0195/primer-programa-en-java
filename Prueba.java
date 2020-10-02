5a6bce1f7e9bcc657cc076c70c30ecbd4837464a
Luis Martínez Alonso <luis0pk0rgmail.com>
2020-10-02 09:39:04 +0200

    Nombre, lugar de nacimiento y año

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String args[]) {
 
 		System.out.println("Luis Martínez Alonso");
+		System.out.println("Lugar de nacimiento: León, Año: 2002");
 
 	}
 
